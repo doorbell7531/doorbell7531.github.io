86630bc4583e0600b34c26f48009f89a04d4f165
Doorbell <doorbell7531@gmail.com>
2019-04-08 15:56:25 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -7,12 +7,14 @@ const fbPromise: Promise<void> = $.getScript('https://connect.facebook.net/zh_TW
 });
 
 $('a.Login_Scope').click(()=>OnResolved(()=>{
-    FB.login((response:any)=>
+    FB.login((response:fb.StatusResponse)=>
     {
-        console.log('name = ' + response.name);
-        console.log('email = ' + response.email);
-        console.log('user_likes = ' + response.user_likes);
-        console.info('1');
+        console.log(response.status);
+        
+        // console.log('name = ' + response.name);
+        // console.log('email = ' + response.email);
+        // console.log('user_likes = ' + response.user_likes);
+        // console.info('1');
 
     },{scope:'email,user_likes,user_friends,user_location,user_posts,user_videos'});
 }));
