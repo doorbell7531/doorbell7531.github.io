64ad74fdba990668d6366d296d5ddece56676f87
Doorbell <doorbell7531@gmail.com>
2019-04-08 15:37:37 +0800

    輸出用戶訊息

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,20 @@
 const fbPromise: Promise<void> = $.getScript('https://connect.facebook.net/zh_TW/sdk.js').done(()=>{
     FB.init({
-        appId: '817081745317047',
+        appId: '817081745317047',   //測試用APP - Test 1
         xfbml: true,
         version: 'v3.2'
     });
 });
 
-$('a.login').click(()=>OnResolved(()=>{
-    FB.login((response:fb.StatusResponse)=>{
-        console.log(response.status);
-    }
-    ,{
-        scope:'email,user_friends,user_location,user_photos,user_posts'
-    }
-    );
+$('a.Login_Scope').click(()=>OnResolved(()=>{
+    FB.login((response:any)=>
+    {
+        console.log('name = ' + response.name);
+        console.log('email = ' + response.email);
+        console.log('user_likes = ' + response.user_likes);
+        console.info('1');
+
+    },{scope:'email,user_likes,user_friends,user_location,user_posts,user_videos'});
 }));
 
 $('a.logout').click(()=>OnResolved(()=>{
