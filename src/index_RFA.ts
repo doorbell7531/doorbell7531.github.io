fff35e9dacc2e02b52feea13c1f7ea4cfb984fa8
Doorbell <doorbell7531@gmail.com>
2019-05-27 16:17:03 +0800

    reset

--- src/index_RFA.ts
+++
@@ -1,7 +0,0 @@
-import SimulateChat from "./module/SimulateChat/SimulateChat";
-
-const simulateChat:SimulateChat = new SimulateChat($('#main'), "script_RFA_1.xml", ()=>{
-    // console.log('腳本結束');
-});
-
-// simulateChat.Start("script_RFA_1.xml");
--- src/index_unknow.ts
+++
@@ -1,5 +0,0 @@
-import SimulateChat from "./module/SimulateChat/SimulateChat";
-
-const simulateChat:SimulateChat = new SimulateChat($('#main'), "script_unknow.xml", ()=>{
-    // console.log('腳本結束');
-});
